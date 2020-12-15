ffb720dc5c40c240b75010089b2564dbe1d7f2f2
Ahmed.Mansour <azm030@gmail.com>
2020-12-16 00:35:42 +0200

    adjust port

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 
 import App from './app';
 import TransactionController from './transactions/transactions.controller';
-
+const port = process.env.PORT || 5000
 const app = new App([
    new TransactionController()
-], 5000)
+], port)
 
 
 app.listen();
